e4501f5687a84a4f078ae7cd8c81fe27ee255f17
jeong in seo <128437656+GEUMAIN@users.noreply.github.com>
2023-05-17 18:06:06 +0900

    네번째 문제 파일
    네번째 문제 파일

---
+++ 5월17일 자바수업/Ex04.java
@@ -0,0 +1,26 @@
+package Test;
+
+import java.util.Scanner;
+
+public class Ex04 {
+// 어떤 정수 n을 입력받고,
+// 1부터 홀수를 차례대로 더해 나가면서 합이 n보다 크면 그 때까지 더한 홀수의 개수와 그 합
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("값을 입력하세요.");
+	int n = sc.nextInt();
+	int sum = 0;
+	int cnt = 0;
+	int i;
+	for (i=1; ; i++) {
+		if(i%2==1) {
+			sum=sum+i;
+			cnt++;
+		}
+		if (sum > n) {
+			break;
+		}
+	}
+	System.out.println(cnt + ","+ sum);
+	}
+}
