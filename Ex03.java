512bead83f7215e79d0078b34facfa18656a21e2
jeong in seo <128437656+GEUMAIN@users.noreply.github.com>
2023-06-01 18:27:47 +0900

    자바 공부51 파일
    자바 공부51 파일

---
+++ Ex03.java
@@ -0,0 +1,33 @@
+package arrayTest;
+
+import java.util.Scanner;
+
+public class Ex03 {
+	//int[] arr = newint[크기];
+	//배열 순회
+	//for(int i=0; i<arr.length; i++){}
+	
+	//최대 100까지의 정수를 차례로 입력 받다가
+	//0이 입력되면 입력을 중단하고,
+	//짝수번째 입력된 정수를 모두 출력하는 프로그램 작성
+	
+	// 5 9 12 16 78 18 98 35 15 0
+	//9 16 18 35
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] num = new int[100];
+		int cnt =0;
+		System.out.println("100가지 숫자를 입력하세요");
+		Loop1:
+		for (int i = 0; i<num.length; i++) {
+			num[i] = sc.nextInt();
+			if(num[i]==0) {
+				break Loop1;
+			}
+			cnt++;
+		}
+		for(int i =0; i<cnt; i+=2) {
+			System.out.print(num[i+1]+" ");
+		}
+	}
+}
