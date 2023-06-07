1630d6f494a0f2d1240e3a3e36b1e0a7e67f5e94
jeong in seo <128437656+GEUMAIN@users.noreply.github.com>
2023-06-07 18:21:27 +0900

    자바 공부54 파일
    자바 공부54 파일

--- Ex06.java
+++ Ex06.java
@@ -1,24 +1,25 @@
-package test;
+package arrayTest;
 
 import java.util.Scanner;
 
 public class Ex06 {
+	
+	// 10개의 정수를 입력받아 nums에 저장한다.
+	//입력받은 10개의 정수 중 가장 큰 수를 출력하는 프로그램을 생성
+	
 	public static void main(String[] args) {
-		//팩토리얼 계산기
-		//5! = 5*4*3*2*1
-		//어떤 수 num를 입력받으면 그 수의 팩토리얼을 출력!!
 		Scanner sc = new Scanner(System.in);
-		int fact = 1; //누적곱을 구할 땐 항상 1로 초기화
-		//int sum = 0; 누적합을 구할 땐 항상 0로 초기화
-		int num = sc.nextInt();
-//		for (int i = 1; i<=num; i++) {
-//			fact= fact*i;
-//		}
-//		System.out.println(fact);
-		while ( num > 1) {
-			fact = fact*num;
-			num--;
+		int max;
+		int[] nums = new int[10];
+		for(int i =0; i<nums.length; i++) {
+			nums[i] = sc.nextInt();
+			}
+		max = nums[0];
+		for(int i = 0; i<nums.length; i++) {
+			if(max < nums[i]) {
+				max = nums[i];
+			}
 		}
-		System.out.println(fact);
+		System.out.printf("최댓값은 %d입니다\n",max);
 	}
 }
