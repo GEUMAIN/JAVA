046f3101ea8ed2d978bb2f46d0c758380be4925c
jeong in seo <128437656+GEUMAIN@users.noreply.github.com>
2023-06-13 17:00:22 +0900

    자바 공부58 파일
    자바 공부58 파일

---
+++ _04_ParameterAndReturn.java
@@ -0,0 +1,27 @@
+package chap_06;
+
+public class _04_ParameterAndReturn {
+		public static int getPower(int num) { // num -> Parameter, 매개변수
+			int result = num * num;
+			return result;
+		}
+		
+		public static int getPowerByExp(int num, int exp) {
+			int result = 1;
+			for (int i=0; i < exp; i++) {
+				result = result * num;
+			}
+			return result;
+		}
+		
+		public static void main(String[] args) {
+			
+			int val = getPower(5);
+			System.out.println(val);
+			
+			System.out.println(getPower(7));
+			
+			val = getPowerByExp(4, 3);
+			System.out.println(val);
+		}
+	}
