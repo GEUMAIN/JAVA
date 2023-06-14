03f14020772e2916181cfba219916d85bc99d664
jeong in seo <128437656+GEUMAIN@users.noreply.github.com>
2023-06-14 18:04:02 +0900

    BlackBox 파일
    BlackBox 파일

---
+++ 클래스 공부/파일/BlackBox.java
@@ -0,0 +1,14 @@
+package chap_07;
+
+public class BlackBox {
+	
+	// 클래스의 구성 요소 - 필드
+	String modelName; //모델명
+	String resolution; //해상도
+	int price; //가격
+	String color; //색상
+	
+//	boolean canAutoReport; //자동신고기능
+//	모든 객체에 공통적으로 필요한 변수는 클래스로 변수를 생성한다.
+	static boolean canAutoReport = false;
+}
