c4ea6b261bc27f61532bb2e16028f9cc10c787ba
jeong in seo <128437656+GEUMAIN@users.noreply.github.com>
2023-06-20 17:59:15 +0900

    Add files via upload

--- 클래스 공부/파일/BlackBox.java
+++ 클래스 공부/파일/BlackBox.java
@@ -1,5 +1,4 @@
 package chap_07;
-
 public class BlackBox {
 	
 	// 클래스의 구성 요소 - 필드
@@ -24,5 +23,38 @@ public class BlackBox {
 	void insertMemoryCard(int capacity) {
 		System.out.println("메모리 카드가 삽입되었습니다.");
 		System.out.println("용량은 " + capacity + " GB 입니다.");
+	}
+
+	int getVideoFileCount(int type) {
+		if(type == 1){ //일반영상
+		return 9;
+		} else if(type == 2) { //이벤트 영상
+		return 2;
+		}
+		return 11;
+	}
+
+	//ShowDateTime : 날짜정보를 표시 여부
+	//ShowSpeed : 속도정보 표시 여부
+	// min : 영상 기록 단위(분)
+	void record(boolean showDateTime, boolean showSpeed, int min) {
+		System.out.println("녹화를 시작합니다.");
+		if (showDateTime == true) {
+			System.out.println("영상의 날짜/시간 정보가 표시됩니다.");
+		}
+		if (showSpeed == true) {
+			System.out.println("영상의 속도 정보가 표시됩니다.");
+		}
+		System.out.println("영상은 " + min + "분 단위로 기록됩니다.");
+	}
+	
+	// 사용자가 조작하지 않은 기본상태는 모두 활성화되고 5분 간격으로 녹화를 한다.
+	// 오버로딩
+	void record(int min) {
+		record(true,true,min);
+		
+	}
+	void record() {
+		record(true,true,5);
 	}
 }
