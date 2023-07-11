936dfa62ba333aaf662f1716b7b29aa664103465
jeong in seo <128437656+GEUMAIN@users.noreply.github.com>
2023-07-11 17:48:16 +0900

    예시문제6_1 파일
    예시문제6_1 파일

---
+++ 시험 대비 복습/Ex06_1.java
@@ -0,0 +1,17 @@
+package test2;
+
+public class Ex06_1 {
+	
+	public static void main(String[] args) {
+		int result = factorial(5);
+		System.out.println(result);
+		
+	}
+	public static int factorial(int num) {
+		if( num == 1) {
+			return 1;
+		}
+		
+		return num * factorial(num-1);
+	}
+}
