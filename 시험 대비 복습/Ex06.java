30a6e2e1155d3971f04e290aa8f82f4c0aadd8e4
jeong in seo <128437656+GEUMAIN@users.noreply.github.com>
2023-07-11 17:47:40 +0900

    예시문제6 파일
    예시문제6 파일

---
+++ 시험 대비 복습/Ex06.java
@@ -0,0 +1,20 @@
+package test2;
+
+import java.util.Scanner;
+
+public class Ex06 {
+
+	// 팩토리얼 계산기
+	// 어떤 수 num를 입력받으면 그 수의 팩토리얼을 출력!!
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int fact = 1;
+		int num = sc.nextInt();
+		while (num > 1) {
+			fact = fact * num;
+			num--;
+		}
+		System.out.println(fact);
+	}
+}
