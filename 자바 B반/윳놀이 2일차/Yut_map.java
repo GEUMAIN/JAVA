9ad82cd6d3ada0b24b19583704e49d0d0bc809c2
jeong in seo <128437656+GEUMAIN@users.noreply.github.com>
2023-10-23 18:31:41 +0900

    Add files via upload

---
+++ 자바 B반/윳놀이 2일차/Yut_map.java
@@ -0,0 +1,65 @@
+package yut_play;
+
+import java.util.Scanner;
+
+public class Yut_map {
+	public static void main(String[] args) {
+		String[] m = {
+				"■", //[0]
+				"○","○","○","○", //[1~4]
+				"■",//[5]
+				"○","○","○","○", //[6~9]
+				"■",//[10]
+				"○","○","○","○",//[11~14]
+				"■",//[15]
+				"○","○","○","○"//[16~19]
+				};
+//		System.out.println(m[10]+" "+m[9]+" "+m[8]+" "+m[7]+" "+m[6]+" "+m[5]);
+//		System.out.println(m[11]+"	  "+m[4]);
+//		System.out.println(m[12]+"	  "+m[3]);
+//		System.out.println(m[13]+"	  "+m[2]);
+//		System.out.println(m[14]+"	  "+m[1]);
+//		System.out.println(m[15]+" "+m[16]+" "+m[17]+" "+m[18]+" "+m[19]+" "+m[0]);
+		System.out.printf("%s \t %s \t %s \t %s \t %s \t %s \n",m[10],m[9],m[8],m[7],m[6],m[5]);
+		System.out.println();
+		System.out.println();
+		System.out.printf("%s \t \t \t \t \t %s \n",m[11],m[4]);
+		System.out.println();
+		System.out.println();
+		System.out.printf("%s \t \t \t \t \t %s \n",m[12],m[3]);
+		System.out.println();
+		System.out.println();
+		System.out.printf("%s \t \t \t \t \t %s \n",m[13],m[2]);
+		System.out.println();
+		System.out.println();
+		System.out.printf("%s \t \t \t \t \t %s \n",m[14],m[1]);
+		System.out.println();
+		System.out.println();
+		System.out.printf("%s \t %s \t %s \t %s \t %s \t %s \n",m[15],m[16],m[17],m[18],m[19],m[0]);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("플레이어 이름을 입력해주세요");
+		String p1Name = sc.next();
+		int p1pos = 0;
+		m[p1pos] = p1Name;
+		
+		System.out.printf("%s \t %s \t %s \t %s \t %s \t %s \n",m[10],m[9],m[8],m[7],m[6],m[5]);
+		System.out.println();
+		System.out.println();
+		System.out.printf("%s \t \t \t \t \t %s \n",m[11],m[4]);
+		System.out.println();
+		System.out.println();
+		System.out.printf("%s \t \t \t \t \t %s \n",m[12],m[3]);
+		System.out.println();
+		System.out.println();
+		System.out.printf("%s \t \t \t \t \t %s \n",m[13],m[2]);
+		System.out.println();
+		System.out.println();
+		System.out.printf("%s \t \t \t \t \t %s \n",m[14],m[1]);
+		System.out.println();
+		System.out.println();
+		System.out.printf("%s \t %s \t %s \t %s \t %s \t %s \n",m[15],m[16],m[17],m[18],m[19],m[0]);
+	}
+	
+}
